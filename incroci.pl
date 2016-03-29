f3c34294221fc1bb9307446d8da63529bcf883c1
Giuseppe <giuseppe.rizzi.88@hotmail.it>
2016-03-29 17:58:39 +0200

    TO-DO: How to determine which bound-free vehicle is first to go, after a tram?

--- incroci.pl
+++ incroci.pl
@@ -36,83 +36,85 @@
 :- discontiguous procede/2.
 
 
+%Segnali di precedenza
+
+%TO-DO: Add other road signs
+segnale(dare_precedenza) :-
+	segnale(precedenza).
+
+segnale(stop) :-
+	segnale(precedenza).
+
+
 %Normally, vehicle facts are pointless. Here they are placed just as a remainder the crossroad components
 
 %Vehicle atoms are identified with the same letter in exercises glued together with the exercise number
 
-%es1
-veicolo(b1).
-veicolo(r1).
-veicolo(n1).
-veicolo(d1).
+%%es1
+%da_destra(veicolo(b1), veicolo(r1)).
+%da_destra(veicolo(r1), veicolo(n1)).
 
-da_destra(veicolo(b1), veicolo(r1)).
-da_destra(veicolo(r1), veicolo(n1)).
-
-procede(veicolo(b1), sinistra).
-procede(veicolo(r1), dritto).
-procede(veicolo(n1), dritto).
-procede(veicolo(d1), sinistra).
+%procede(veicolo(b1), sinistra).
+%procede(veicolo(r1), dritto).
+%procede(veicolo(n1), dritto).
+%procede(veicolo(d1), sinistra).
 
 
-%es2
-veicolo(b2).
-veicolo(c2).
-veicolo(h2).
+%%es2
+%da_destra(veicolo(b2), veicolo(h2)).
+%da_destra(veicolo(h2), veicolo(c2)).
 
-da_destra(veicolo(b2), veicolo(h2)).
-da_destra(veicolo(h2), veicolo(c2)).
-
-procede(veicolo(b2), dritto).
-procede(veicolo(c2), dritto).
-procede(veicolo(h2), sinistra).
+%procede(veicolo(b2), dritto).
+%procede(veicolo(c2), dritto).
+%procede(veicolo(h2), sinistra).
 
 
-%es3
-veicolo(r3).
-veicolo(b3).
-veicolo(d3).
-veicolo(t3).
-
-da_destra(veicolo(r3), veicolo(b3)).
-da_destra(veicolo(b3), veicolo(d3)).
-da_destra(veicolo(d3), veicolo(t3)).
+%%es3
+%da_destra(veicolo(r3), veicolo(b3)).
+%da_destra(veicolo(b3), veicolo(d3)).
+%da_destra(veicolo(d3), veicolo(t3)).
 
 
-%es4
-veicolo(a4).
-veicolo(f4).
-veicolo(l4).
-veicolo(h4).
+%%es4
+%da_destra(veicolo(h4), veicolo(a4)).
+%da_destra(veicolo(a4), veicolo(f4)).
+%da_destra(veicolo(f4), veicolo(l4)).
 
-da_destra(veicolo(h4), veicolo(a4)).
-da_destra(veicolo(a4), veicolo(f4)).
-da_destra(veicolo(f4), veicolo(l4)).
-
-procede(veicolo(h4), destra).
-procede(veicolo(a4), sinistra).
-procede(veicolo(f4), destra).
-procede(veicolo(l4), sinistra).
+%procede(veicolo(h4), destra).
+%procede(veicolo(a4), sinistra).
+%procede(veicolo(f4), destra).
+%procede(veicolo(l4), sinistra).
 
 
 %es12
-veicolo(b12).
-veicolo(tram).
-veicolo(g12).
-veicolo(p12).
 
-%%-----TO-DO: Implement signals
+%%-----TO-DO: Implement signs
 
-rispetta(veicolo(b12), segnale(dare_precedenza)).
-rispetta(veicolo(p12), segnale(dare_precedenza)).
+deve_rispettare(veicolo(b12), segnale(dare_precedenza)).
+deve_rispettare(veicolo(p12), segnale(dare_precedenza)).
+
+proviene(veicolo(b12), braccio_nord).
+proviene(veicolo(g12), braccio_ovest).
+proviene(veicolo(p12), braccio_sud).
+
+procede(veicolo(b12), sinistra, braccio_est).
+procede(veicolo(p12), dritto, braccio_nord).
+procede(veicolo(g12), dritto, braccio_est).
+%procede(veicolo(tram), _, _).
 
 %Rules
-veicolo(in_soccorso) :-
-	veicolo(tram);
-	veicolo(ambulanza);
-	veicolo(antincendio);
-	veicolo(polizia).
+precedenza(veicolo(tram), _).
 
+%--TO-DO: Come diavolo faccio a dire che g12 è primo, dopo il tram???
+
+%Precedenza regolata da segnali
+precedenza(veicolo(X), veicolo(Y)) :-
+	precedenza_frontale(veicolo(X), veicolo(Y)).
+
+precedenza(veicolo(_), veicolo(Y)) :-
+	deve_rispettare(veicolo(Y), segnale(precedenza)).
+
+%Precedenza a destra
 precedenza(veicolo(X), veicolo(Y)) :-
 	da_destra(veicolo(X), veicolo(Y)).
 
@@ -120,13 +122,24 @@ precedenza(veicolo(X), veicolo(Y)) :-
 	da_destra(veicolo(X), veicolo(Z)),
 	precedenza(veicolo(Z), veicolo(Y)).
 
-precedenza(veicolo(in_soccorso), _).
-precedenza(veicolo(tram), _).
+%Precedenza_frontale
+precedenza_frontale(veicolo(X), veicolo(Y)) :-
+	procede(veicolo(X), destra, Braccio),
+	procede(veicolo(Y), sinistra, Braccio).
 
+precedenza_frontale(veicolo(X), veicolo(Y)) :-
+	proviene(veicolo(Y), BraccioY),
+	procede(veicolo(X), dritto, BraccioY),
+	procede(veicolo(Y), sinistra, _).
+		
+
+%Sinistri --Da rivedere
 sinistro(veicolo(X), veicolo(Y)) :-
+	X \== Y,
 	scontro(veicolo(X), veicolo(Y)).
 
 sinistro(veicolo(X), veicolo(Y)) :-
+	X \== Y,
 	scontro(veicolo(Y), veicolo(X)).
 
 scontro(veicolo(X), veicolo(Y)) :-
@@ -143,4 +156,4 @@ scontro(veicolo(X), veicolo(Y)) :-
 	procede(veicolo(X), dritto),
 	procede(veicolo(Y), dritto).
 
-
+%--TO-DO: l'ordine delle regole dovrebbe (FORSE) rispettare la priorità dei segnali: vigile, semaforo, verticali, orizzontali.
